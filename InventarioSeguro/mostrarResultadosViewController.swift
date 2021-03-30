613ec69ba3f40b687f3dba1f7902c61ce850975d
jorgeramos2 <jorge.ramos1996@gmail.com>
2021-03-30 17:09:52 -0600

    base
    Contiene las funcionalidad basica de tomar foto y reconocer la imagen

---
+++ InventarioSeguro/mostrarResultadosViewController.swift
@@ -0,0 +1,29 @@
+//
+//  mostrarResultadosViewController.swift
+//  InventarioSeguro
+//
+//  Created by Jorge Ramos on 30/03/21.
+//
+
+import UIKit
+
+class mostrarResultadosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
